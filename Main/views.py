d675bf8ca0d0fe6e8eb912c57ecc2a5be086ee36
dannyinVT <68930250+dannyinVT@users.noreply.github.com>
2021-04-14 09:33:35 -0400

    Portfolio built using Django, Python, Bootstrap

---
+++ Main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'Main/home.html')
+
+
+def index(request):
+    return render(request, 'Main/index.html')
+
